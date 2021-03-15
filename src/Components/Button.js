8ec8fa0c71224a6ad91dbce130b3646c63085462
Deveshb15 <deveshb583@gmail.com>
2021-03-15 19:48:21 +0530

    Add task

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 const Button = (props) => {
-    return <button style={{ backgroundColor: props.color }} className="btn" onClick={props.btnClick}>{props.text}</button>
+    return <button style={{ backgroundColor: props.color }} className="btn" onClick={props.onClick}>{props.text}</button>
 }
 
 Button.defaultProps = {
