b5253e9bfc50e66a661e128f9e633e5d77f8b1f4
Deveshb15 <deveshb583@gmail.com>
2021-03-16 20:16:04 +0530

    Merge conflicts solved

--- src/App.js
+++ src/App.js
@@ -9,35 +9,20 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
-<<<<<<< HEAD
-        name: 'Write a book',
-        age: '13 March',
-=======
         text: 'Write a Blog',
         day: '14 March',
->>>>>>> 593d7fddc7e563a2244094025f0e3bd965cd9aa9
         id:1,
         reminder: true
     },
     {
-<<<<<<< HEAD
-        name: 'Don\'t write a book',
-        age: '14 March',
-=======
         text: 'Create TODO',
         day: '14 March',
->>>>>>> 593d7fddc7e563a2244094025f0e3bd965cd9aa9
         id:2,
         reminder: true
     },
     {
-<<<<<<< HEAD
-        name: 'Read something',
-        age: '15 March',
-=======
         text: 'Read Sapiens',
         day: '14 March',
->>>>>>> 593d7fddc7e563a2244094025f0e3bd965cd9aa9
         id:3,
         reminder: false
     }
